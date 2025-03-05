34625c966ef7b0d3865928c6e46fedaa92619bf2
RaymondBog <raymondbog@gmail.com>
2025-03-05 13:00:41 +0000

    fixed loop

--- index.py
+++ index.py
@@ -1,14 +1,13 @@
 import random
-choice=input("Roll a dice? (y/n): ").lower()
-if choice == "y":
-    roll1 =random.randint(1,6)
-    roll2 =random.randint(1,6)
-    print(f"({roll1},{roll2})")
-elif choice == "n":
-    print("Thanks for playing")
-else:
-    print("invalid format")  
-    
 
-
-
+while True:
+    choice = input("Roll a dice? (y/n): ").lower()
+    if choice == "y":
+        roll1 = random.randint(1, 6)
+        roll2 = random.randint(1, 6)
+        print(f"({roll1},{roll2})")
+    elif choice == "n":
+        print("Thanks for playing")
+        break
+    else:
+        print("Invalid format. Please enter 'y' or 'n'.")
